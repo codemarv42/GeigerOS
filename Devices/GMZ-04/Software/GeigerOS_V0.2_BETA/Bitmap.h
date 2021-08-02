b09963e9c7cc200339ab7f8bf4c31afca57fd2ed
Marvin Ramm <88335544+codemarv42@users.noreply.github.com>
2021-08-02 18:07:25 +0200

    Add files via upload

---
+++ Devices/GMZ-04/Software/GeigerOS_V0.2_BETA/Bitmap.h
@@ -0,0 +1,16 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+extern const unsigned char wifiBitmap[];
+extern const unsigned char settingsBitmap[];
+extern const unsigned char buzzerOnBitmap[];
+extern const unsigned char buzzerOffBitmap[];
+extern const unsigned char ledOnBitmap[];
+extern const unsigned char ledOffBitmap[];
+extern const unsigned char backBitmap[];
+extern const unsigned char geigerCounterStartPageBitmap[];
+extern const unsigned char bluetoothBitmap[];
+extern const unsigned char bluetoothConnectedBitmap[];
+extern const unsigned char sdCardBitmap[];
+
+#endif
