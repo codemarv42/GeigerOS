1c5a2d78a8178e74eeba23f8812d54b41aacfb16
Marvin Ramm <88335544+codemarv42@users.noreply.github.com>
2023-07-29 10:05:23 +0200

    Add files via upload

---
+++ Software/GeigerOS/GMZ_06_Gläsernes_Labor/GeigerOS_V1.20/Settings.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define GEIGEROS_VERSION 1.20
+#define GMZ_MODEL_NR     6
+#define LOG              0
+#define SERIAL           1  //Activate / Deactivate Serial
+#define RESTRICTED_MODE  1  //0 - OFF, 1 - ON
+
+#endif
